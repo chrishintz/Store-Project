c8c8a451da9cf167811d7af3a383f44fae30bbd9
Chris Hintz <chrishintz42@gmail.com>
2015-12-02 12:48:22 -0800

    Add Bootstrap CSS and JS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
 
+  def create
+      # Should the below be password_digest or password and password_confirmation?
+    @user = User.new(params.require(:users).permit(:email, :password_digest))
+    @user.save
+    session[:user_id] = @user.id
+    redirect_to home_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get "/",        to:"products#index"
+  get "/",        to: "products#index" as: :home
+  post "/users",  to: "users#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
