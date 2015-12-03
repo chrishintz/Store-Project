b573a46684ff46c63382765d7025eea0c2ec1849
Chris Hintz <chrishintz42@gmail.com>
2015-12-02 16:13:40 -0800

    Added sign-up validations functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,9 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to home_path
+      redirect_to home_path, notice: "Logged in!"
     else
+      flash.now.alert = "Email or password is invalid"  
       redirect_to home_path
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
-    @user.save
-    session[:user_id] = @user.id
-    redirect_to home_path
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to home_path, notice: "Thank you for signing up!"
+    else
+      render "index"
+    end
   end
 end
