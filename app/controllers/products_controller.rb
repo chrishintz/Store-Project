192dabb47edb8f310bc203f7990ed0ea4c29f3b5
Chris Hintz <chrishintz42@gmail.com>
2015-12-04 11:16:38 -0800

    Create Routes and Method to set-up product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,22 @@ class ProductsController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @products           = Product.all
+  end
+
+  def create
+    @product = Product.new(params.require(:product).permit(:name, :description, :image_url))
+    if @product.save
+      redirect_to products_path
+    else
+      render "index"
+    end
+  end
+
+  private
+
+  def log_params
+    params.require(:product).permit(:name, :description, :image_url)
+  end
 end
