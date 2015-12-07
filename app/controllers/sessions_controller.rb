5b4f113edc83cb5ea0c79cd0228f64a191dc5916
Chris Hintz <chrishintz42@gmail.com>
2015-12-07 13:54:18 -0800

    Create notice alert for signing out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,16 @@ class SessionsController < ApplicationController
 
   def sign_out
     session.delete(:user_id)
-    redirect_to "/"
+    redirect_to "/", notice: "You were signed out."
   end
 
   def sign_in
     @user = User.find_by(email: params[:email])
     if @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to home_path, notice: "Logged in!"
+      redirect_to home_path
     else
-      flash.now.alert = "Email or password is invalid"  
+      flash.now.alert = "Email or password is invalid"
       redirect_to home_path
     end
   end
