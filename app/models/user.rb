475c1b4dab6accece55139a06f4b1d6d60581057
Chris Hintz <chrishintz42@gmail.com>
2015-12-07 14:12:15 -0800

    Add 'has many' & 'belongs_to' relationships for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  has_many :orders
   # attr_accessable :email, :password, :password_confirmation
   validates :email, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
